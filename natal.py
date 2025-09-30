ce3eed4093ec2bdda1d7291b5cca603f931362e8
Gabriele Alves <bihalves691@gmail.com>
2025-09-30 16:49:12 -0300

    questao 1, unidade 5

---
+++ natal.py
@@ -0,0 +1,9 @@
+from cartaoweb import CartaoWeb
+
+class Natal(CartaoWeb):
+   def __init__(self, destinatario):
+      super().__init__(destinatario)
+    
+   def showMessage(self):
+       print(f"Feliz Natal, {self.destinatario}! Que a mágia do Natal aqueça seu coração.")
+       print("-"*35)
