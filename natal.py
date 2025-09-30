079a5c4f96b2059615bea5f7431b417e3f156be8
Gabriele Alves <bihalves691@gmail.com>
2025-09-30 16:49:52 -0300

    Update natal.py

--- natal.py
+++ natal.py
@@ -1,9 +1,1 @@
-from cartaoweb import CartaoWeb
 
-class Natal(CartaoWeb):
-   def __init__(self, destinatario):
-      super().__init__(destinatario)
-    
-   def showMessage(self):
-       print(f"Feliz Natal, {self.destinatario}! Que a mágia do Natal aqueça seu coração.")
-       print("-"*35)
